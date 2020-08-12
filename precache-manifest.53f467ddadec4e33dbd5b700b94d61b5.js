d895ab6de426ad643cdc028125ea875b11dd991b
Sarita <dsarritta@gmail.com>
2020-08-13 01:15:41 +0545

    Updates

---
+++ precache-manifest.53f467ddadec4e33dbd5b700b94d61b5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10930a9c2a9198879fcf1502298e22f8",
+    "url": "/React-portfolio/index.html"
+  },
+  {
+    "revision": "b475ad58e232cecf52cc",
+    "url": "/React-portfolio/static/css/main.b3ce2447.chunk.css"
+  },
+  {
+    "revision": "2e8904edb8c4f9051f54",
+    "url": "/React-portfolio/static/js/2.1cbfa2da.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-portfolio/static/js/2.1cbfa2da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b475ad58e232cecf52cc",
+    "url": "/React-portfolio/static/js/main.68a3a500.chunk.js"
+  },
+  {
+    "revision": "a93d179556bca06183fc",
+    "url": "/React-portfolio/static/js/runtime-main.b79e20e6.js"
+  },
+  {
+    "revision": "a73a8fb70a20092194dff2a837440e55",
+    "url": "/React-portfolio/static/media/profile.a73a8fb7.jpg"
+  },
+  {
+    "revision": "8c2cdc7c49e1d7fb4ad616ddc8b0a41e",
+    "url": "/React-portfolio/static/media/river.8c2cdc7c.jpg"
+  }
+]);
