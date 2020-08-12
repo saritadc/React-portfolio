b2130752b5542a9d1197db02094444c2e514475a
Sarita <dsarritta@gmail.com>
2020-08-12 21:11:30 +0545

    add handleInput and addItem function in toDo file

--- src/components/todo/toDo.jsx
+++ src/components/todo/toDo.jsx
@@ -5,16 +5,37 @@ class ToDo extends React.Component {
     super(props);
     this.state={
       items: [],
-      currentCurrent:{}
+      currentItem:{
+        text:'',
+        key:''
+      }
     }
+    this.handleInput = this.handleInput.bind(this);
+    this.addItem = this.addItem.bind(this);
+  }
+  
+  handleInput(e){
+    this.setState({
+      currentItem:{
+        text: e.target.value,
+        key: Date.now
+      }
+    })
+  }
+
+  addItem(e){
+    e.preventDefault();
+    const newItem = this.state.currentItem;
+    console.log(newItem);
   }
 
   render() {
     return (
       <div className="todo-container">
         <h1>My To Do List</h1>
-        <form id="todo-form">
-          <input type="text" placeholder="enter task"/>
+        <form id="todo-form" onSubmit={this.addItem}>
+          <input type="text" placeholder="enter task" value={this.state.currentItem.text}
+          onChange={this.handleInput}/>
           <button type="submit">Add</button>
         </form>
       </div>
